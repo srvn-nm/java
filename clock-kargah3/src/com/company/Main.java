c17703039c4f23628387470f516a1c41bc809620
Sarvin Nami <srvn0nm@gmail.com>
2021-10-13 18:32:42 +0330

    editting

--- clock-kargah3/src/com/company/Main.java
+++ clock-kargah3/src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException{
         ClockDisplay clockDisplay = new ClockDisplay();
         while (true){
-            Thread.sleep(1000);
+            Thread.sleep(100);
             clockDisplay.timeTick();
             System.out.println(clockDisplay.getTime());
         }
