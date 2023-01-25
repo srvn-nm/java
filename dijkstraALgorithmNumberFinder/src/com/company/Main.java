f448ce2d3b0e1a91fe3a9198a52b6fce91a6fd78
Sarvin Nami <srvn0nm@gmail.com>
2023-01-25 13:00:47 +0330

    debugging

--- dijkstraALgorithmNumberFinder/src/com/company/Main.java
+++ dijkstraALgorithmNumberFinder/src/com/company/Main.java
@@ -18,20 +18,9 @@ public class Main {
         }
 
         for (int i = 0; i < M; i++) {
-            Scanner sc2 = new Scanner(System.in);
             String[] line2 = sc.nextLine().split(" ");
             com.company.GFG.addEdge(adj, Integer.parseInt(line2[0]), Integer.parseInt(line2[1]), Integer.parseInt(line2[2]));
         }
-//        com.company.GFG.addEdge(adj, 0, 6, 7);
-//        com.company.GFG.addEdge(adj, 0, 1, 2);
-//        com.company.GFG.addEdge(adj, 1, 2, 3);
-//        com.company.GFG.addEdge(adj, 1, 3, 3);
-//        com.company.GFG.addEdge(adj, 6, 3, 3);
-//        com.company.GFG.addEdge(adj, 3, 5, 1);
-//        com.company.GFG.addEdge(adj, 6, 5, 1);
-//        com.company.GFG.addEdge(adj, 2, 5, 1);
-//        com.company.GFG.addEdge(adj, 0, 4, 5);
-//        com.company.GFG.addEdge(adj, 4, 6, 2);
 
         com.company.GFG.findShortestPaths(adj, N-1);
     }
@@ -64,16 +53,16 @@ class GFG {
         adj.get(y).add(new Node(x, w));
     }
 
-    static void dijkstra(ArrayList<ArrayList<Node> > adj, int src, int n, int[] dist, int[] paths) {
+    static void dijkstra(ArrayList<ArrayList<Node>> adj, int n, int[] dist, int[] paths) {
 
         PriorityQueue<Node> pq = new PriorityQueue<>(n + 1, new Node());
 
         Set<String> settled = new HashSet<>();
 
-        pq.add(new Node(src, 0));
+        pq.add(new Node(0, 0));
 
-        dist[src] = 0;
-        paths[src] = 1;
+        dist[0] = 0;
+        paths[0] = 1;
 
         while (!pq.isEmpty()) {
 
@@ -121,7 +110,7 @@ class GFG {
         for (int i = 0; i <= n; i++)
             paths[i] = 0;
 
-        dijkstra(adj, 0, n, dist, paths);
+        dijkstra(adj, n, dist, paths);
 
         System.out.print(paths[n]);
     }
